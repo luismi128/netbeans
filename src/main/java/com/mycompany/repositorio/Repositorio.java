79d104a9a91fd7adce8ca3b8674c2b707498276b
Luismi <luismigueldelapuente008@gmail.com>
2023-10-19 15:06:31 +0200

    primer commit

---
+++ src/main/java/com/mycompany/repositorio/Repositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorio;
+
+/**
+ *
+ * @author luismi
+ */
+public class Repositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
